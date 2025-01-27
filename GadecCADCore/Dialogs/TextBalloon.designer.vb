0c2540545031f88dc92528d971a8af67b9c0ad7c
wilbird <wilbertholkema@outlook.com>
2025-01-27 19:11:42 +0100

    GAA-002: Initial upgrade side-by-side

---
+++ GadecCADCore/Dialogs/TextBalloon.designer.vb
@@ -0,0 +1,204 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class TextBalloon
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(TextBalloon))
+        Me.cDescr1 = New System.Windows.Forms.Label()
+        Me.cDescr2 = New System.Windows.Forms.Label()
+        Me.cDescr3 = New System.Windows.Forms.Label()
+        Me.cClient1 = New System.Windows.Forms.Label()
+        Me.cClient2 = New System.Windows.Forms.Label()
+        Me.cClient3 = New System.Windows.Forms.Label()
+        Me.cClient4 = New System.Windows.Forms.Label()
+        Me.cLastRev_Char = New System.Windows.Forms.Label()
+        Me.cLastRev_Date = New System.Windows.Forms.Label()
+        Me.cLastRev_Descr = New System.Windows.Forms.Label()
+        Me.cProject = New System.Windows.Forms.Label()
+        Me.cDossier = New System.Windows.Forms.Label()
+        Me.cScale = New System.Windows.Forms.Label()
+        Me.cDescr4 = New System.Windows.Forms.Label()
+        Me.cFilename = New System.Windows.Forms.Label()
+        Me.BackPictureBox = New System.Windows.Forms.PictureBox()
+        CType(Me.BackPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'cDescr1
+        '
+        resources.ApplyResources(Me.cDescr1, "cDescr1")
+        Me.cDescr1.BackColor = System.Drawing.Color.Black
+        Me.cDescr1.ForeColor = System.Drawing.Color.White
+        Me.cDescr1.Name = "cDescr1"
+        '
+        'cDescr2
+        '
+        resources.ApplyResources(Me.cDescr2, "cDescr2")
+        Me.cDescr2.BackColor = System.Drawing.Color.Black
+        Me.cDescr2.ForeColor = System.Drawing.Color.White
+        Me.cDescr2.Name = "cDescr2"
+        '
+        'cDescr3
+        '
+        resources.ApplyResources(Me.cDescr3, "cDescr3")
+        Me.cDescr3.BackColor = System.Drawing.Color.Black
+        Me.cDescr3.ForeColor = System.Drawing.Color.White
+        Me.cDescr3.Name = "cDescr3"
+        '
+        'cClient1
+        '
+        resources.ApplyResources(Me.cClient1, "cClient1")
+        Me.cClient1.BackColor = System.Drawing.Color.Black
+        Me.cClient1.ForeColor = System.Drawing.Color.White
+        Me.cClient1.Name = "cClient1"
+        '
+        'cClient2
+        '
+        resources.ApplyResources(Me.cClient2, "cClient2")
+        Me.cClient2.BackColor = System.Drawing.Color.Black
+        Me.cClient2.ForeColor = System.Drawing.Color.White
+        Me.cClient2.Name = "cClient2"
+        '
+        'cClient3
+        '
+        resources.ApplyResources(Me.cClient3, "cClient3")
+        Me.cClient3.BackColor = System.Drawing.Color.Black
+        Me.cClient3.ForeColor = System.Drawing.Color.White
+        Me.cClient3.Name = "cClient3"
+        '
+        'cClient4
+        '
+        resources.ApplyResources(Me.cClient4, "cClient4")
+        Me.cClient4.BackColor = System.Drawing.Color.Black
+        Me.cClient4.ForeColor = System.Drawing.Color.White
+        Me.cClient4.Name = "cClient4"
+        '
+        'cLastRev_Char
+        '
+        resources.ApplyResources(Me.cLastRev_Char, "cLastRev_Char")
+        Me.cLastRev_Char.BackColor = System.Drawing.Color.Black
+        Me.cLastRev_Char.ForeColor = System.Drawing.Color.White
+        Me.cLastRev_Char.Name = "cLastRev_Char"
+        '
+        'cLastRev_Date
+        '
+        resources.ApplyResources(Me.cLastRev_Date, "cLastRev_Date")
+        Me.cLastRev_Date.BackColor = System.Drawing.Color.Black
+        Me.cLastRev_Date.ForeColor = System.Drawing.Color.White
+        Me.cLastRev_Date.Name = "cLastRev_Date"
+        '
+        'cLastRev_Descr
+        '
+        resources.ApplyResources(Me.cLastRev_Descr, "cLastRev_Descr")
+        Me.cLastRev_Descr.BackColor = System.Drawing.Color.Black
+        Me.cLastRev_Descr.ForeColor = System.Drawing.Color.White
+        Me.cLastRev_Descr.Name = "cLastRev_Descr"
+        '
+        'cProject
+        '
+        resources.ApplyResources(Me.cProject, "cProject")
+        Me.cProject.BackColor = System.Drawing.Color.Black
+        Me.cProject.ForeColor = System.Drawing.Color.White
+        Me.cProject.Name = "cProject"
+        '
+        'cDossier
+        '
+        resources.ApplyResources(Me.cDossier, "cDossier")
+        Me.cDossier.BackColor = System.Drawing.Color.Black
+        Me.cDossier.ForeColor = System.Drawing.Color.White
+        Me.cDossier.Name = "cDossier"
+        '
+        'cScale
+        '
+        resources.ApplyResources(Me.cScale, "cScale")
+        Me.cScale.BackColor = System.Drawing.Color.Black
+        Me.cScale.ForeColor = System.Drawing.Color.White
+        Me.cScale.Name = "cScale"
+        '
+        'cDescr4
+        '
+        resources.ApplyResources(Me.cDescr4, "cDescr4")
+        Me.cDescr4.BackColor = System.Drawing.Color.Black
+        Me.cDescr4.ForeColor = System.Drawing.Color.White
+        Me.cDescr4.Name = "cDescr4"
+        '
+        'cFilename
+        '
+        resources.ApplyResources(Me.cFilename, "cFilename")
+        Me.cFilename.BackColor = System.Drawing.Color.Black
+        Me.cFilename.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.cFilename.ForeColor = System.Drawing.Color.White
+        Me.cFilename.Name = "cFilename"
+        '
+        'BackPictureBox
+        '
+        Me.BackPictureBox.BackColor = System.Drawing.Color.Black
+        resources.ApplyResources(Me.BackPictureBox, "BackPictureBox")
+        Me.BackPictureBox.Name = "BackPictureBox"
+        Me.BackPictureBox.TabStop = False
+        '
+        'TextBalloon
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ControlBox = False
+        Me.Controls.Add(Me.cFilename)
+        Me.Controls.Add(Me.cDescr4)
+        Me.Controls.Add(Me.cScale)
+        Me.Controls.Add(Me.cDossier)
+        Me.Controls.Add(Me.cProject)
+        Me.Controls.Add(Me.cLastRev_Descr)
+        Me.Controls.Add(Me.cLastRev_Date)
+        Me.Controls.Add(Me.cLastRev_Char)
+        Me.Controls.Add(Me.cClient4)
+        Me.Controls.Add(Me.cClient3)
+        Me.Controls.Add(Me.cClient2)
+        Me.Controls.Add(Me.cClient1)
+        Me.Controls.Add(Me.cDescr3)
+        Me.Controls.Add(Me.cDescr2)
+        Me.Controls.Add(Me.cDescr1)
+        Me.Controls.Add(Me.BackPictureBox)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "TextBalloon"
+        Me.TopMost = True
+        Me.TransparencyKey = System.Drawing.SystemColors.Control
+        CType(Me.BackPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents cDescr1 As System.Windows.Forms.Label
+    Friend WithEvents cDescr2 As System.Windows.Forms.Label
+    Friend WithEvents cDescr3 As System.Windows.Forms.Label
+    Friend WithEvents cClient1 As System.Windows.Forms.Label
+    Friend WithEvents cClient2 As System.Windows.Forms.Label
+    Friend WithEvents cClient3 As System.Windows.Forms.Label
+    Friend WithEvents cClient4 As System.Windows.Forms.Label
+    Friend WithEvents cLastRev_Char As System.Windows.Forms.Label
+    Friend WithEvents cLastRev_Date As System.Windows.Forms.Label
+    Friend WithEvents cLastRev_Descr As System.Windows.Forms.Label
+    Friend WithEvents cProject As System.Windows.Forms.Label
+    Friend WithEvents cDossier As System.Windows.Forms.Label
+    Friend WithEvents cScale As System.Windows.Forms.Label
+    Friend WithEvents cDescr4 As System.Windows.Forms.Label
+    Friend WithEvents cFilename As System.Windows.Forms.Label
+    Friend WithEvents BackPictureBox As Windows.Forms.PictureBox
+End Class
